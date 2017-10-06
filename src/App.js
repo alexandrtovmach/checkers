f9ed209ff2137840380c27ff0c6c9704ea8f6e4d
alexandrtovmach@gmail.com <alexandrtovmach@gmail.com>
2017-10-06 22:34:08 +0300

    cache commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Board } from './components/board/board';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <header className="app-header">
+                    <h1 className="app-title">Easy Checkers</h1>
+                </header>
+                <div className="content">
+                    <Board/>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
