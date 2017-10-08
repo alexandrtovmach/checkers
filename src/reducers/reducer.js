405dd7bd088444fee29a7fd4d0fe856fdbe69455
Alexandr Tovmach <alexandrtovmach@gmail.com>
2017-10-08 17:20:09 +0300

    general functional almost done

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -11,6 +11,9 @@ for (let i = 0; i < 32; i++) {
         victimField: null,
         queen: false
     })
+    if (i===20) {
+        fields[i].queen = true;
+    }
 }
 
 export function game(state = {}, action) {
@@ -19,28 +22,52 @@ export function game(state = {}, action) {
     switch (action.type) {
         case 'CLICK_FIELD': {
             if (state.fields[action.payload].checker) {
-                const fieldArr = clearBoardStatus(state.fields);
+                const fieldArr = clearBoardStatus(state.fields),
+                    steps = getStepsFields(action.payload, state.fields[action.payload].myCheckers, state.fields[action.payload].queen);
 
-                getStepsFields(action.payload, state.fields[action.payload].myCheckers, state.fields[action.payload].queen).forEach(elem => {
-                    const myFields = elem.includes('m');
-                    elem = parseFloat(elem);
+                steps.forEach(elem => {
+                    let myFields;
+                    if (!state.fields[action.payload].queen) {
+                        myFields = elem.includes('m');
+                        elem = parseFloat(elem);
+                    }
                     if (elem >= 0 && elem <= 31) {
                         if (!fieldArr[elem].checker && myFields) {
                             fieldArr[elem].status = 2;
                         } else {
                             getStepsFields(fieldArr[elem].fieldIndex, state.fields[action.payload].myCheckers, state.fields[action.payload].queen).forEach(el => {
-                                el = parseFloat(el);
+                                if (!state.fields[action.payload].queen) {
+                                    el = parseFloat(el);
+                                }
                                 if ((el >= 0 && el <= 31) && 
                                     !fieldArr[el].checker && 
                                     fieldArr[elem].checker && 
-                                    (fieldArr[action.payload].myCheckers !== fieldArr[elem].myCheckers) &&
-                                    (Math.abs(fieldArr[el].fieldIndex - fieldArr[action.payload].fieldIndex) !== 8) &&
-                                    (Math.abs(fieldArr[el].fieldIndex - fieldArr[action.payload].fieldIndex) !== 1)
+                                    (fieldArr[action.payload].myCheckers !== fieldArr[elem].myCheckers)
                                 ) {
-                                    fieldArr[el].victimField = elem;
-                                    fieldArr[el].status = 3;
+                                    if (state.fields[action.payload].queen) {
+                                        if ((steps.indexOf(el) > -1) && 
+                                            (((action.payload > elem) && (el < elem)) ||
+                                            ((action.payload < elem) && (el > elem)))
+                                        ) {
+                                            fieldArr[el].victimField = elem;
+                                            fieldArr[el].status = 3;
+                                        }
+                                    } else if (
+                                        (Math.abs(fieldArr[el].fieldIndex - fieldArr[action.payload].fieldIndex) !== 8) &&
+                                        (Math.abs(fieldArr[el].fieldIndex - fieldArr[action.payload].fieldIndex) !== 1)
+                                    ) {
+                                        fieldArr[el].victimField = elem;
+                                        fieldArr[el].status = 3;
+                                    }
                                 }
                             })
+                        }
+                        if (state.fields[action.payload].queen) {
+                            for (let i = 0; i < steps.length; i++) {
+                                if ((fieldArr[steps[i]].status !== 3)&&(!fieldArr[steps[i]].checker)) {
+                                    fieldArr[steps[i]].status = 2;
+                                }
+                            }
                         }
                     }
                 })
@@ -64,7 +91,7 @@ export function game(state = {}, action) {
             cacheArr[state.activeField.index].checker = false;
             cacheArr[action.payload].checker = true;
             cacheArr[action.payload].myCheckers = state.activeField.myCheckers;
-            cacheArr[action.payload].queen = checkToQueen(action.payload, state.activeField.myCheckers);
+            cacheArr[action.payload].queen = cacheArr[state.activeField.index].queen || checkToQueen(action.payload, state.activeField.myCheckers);
 
             return {
                 ...state,
@@ -80,7 +107,7 @@ export function game(state = {}, action) {
             cacheArr[action.payload.victimField].checker = false;
             cacheArr[action.payload.to].checker = true;
             cacheArr[action.payload.to].myCheckers = state.activeField.myCheckers;
-            cacheArr[action.payload.to].queen = checkToQueen(action.payload, state.activeField.myCheckers);
+            cacheArr[action.payload.to].queen = cacheArr[state.activeField.index].queen || checkToQueen(action.payload.to, state.activeField.myCheckers);
 
             return {
                 ...state,
